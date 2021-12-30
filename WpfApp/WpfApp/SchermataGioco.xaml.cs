5d87dda0382f6c260ac2de2912134303aed8cbe4
Riccardo-Villa <8riccardovilla@gmail.com>
2021-12-30 14:49:58 +0100

    SchermataPerGioco

--- WpfApp/WpfApp/SchermataGioco.xaml.cs
+++ WpfApp/WpfApp/SchermataGioco.xaml.cs
@@ -20,9 +20,59 @@ namespace WpfApp
     public partial class SchermataGioco : Window
     {
         Persone p;
+        List<Button> myButtons = new List<Button>();
+        bool OnScreen = false;
+        const int width = 100;
+        const int height = 25;
+        const int Step = 2;
+        int currentx = 0;
+
+        
         public SchermataGioco()
         {
             InitializeComponent();
+            ButtonArray();
+        }
+        public void ButtonArray()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Button Btn = new Button();
+                Btn.Content = i.ToString();
+                Btn.Name = "Button" + i.ToString();
+                Btn.Width = width;
+                Btn.Height = height;
+                myButtons.Add(Btn);
+
+            }
+
+        }
+        private void Visualizza_Click(object sender, RoutedEventArgs e)
+        {
+            if (!OnScreen)
+            {
+                foreach (var item in myButtons)
+                {
+                    Canvas.SetLeft(item, Step * currentx);
+                    currentx += 50;
+                    MainCanvas.Children.Add(item);
+                }
+                OnScreen = true;
+            }
+        }
+
+        private void Rimuovi_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnScreen)
+            {
+                foreach (var item in myButtons)
+                {
+
+                    MainCanvas.Children.Remove(item);
+                }
+                OnScreen = false;
+                currentx = 0;
+            }
         }
     }
 }
